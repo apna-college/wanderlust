8474cb47f6d1603cdc36c0ea04dd1973843a0d4a
Abhinandan Sah <118913466+Abhinandan-Sah@users.noreply.github.com>
2024-03-21 01:27:41 +0530

    Update listing.js
    Changing image type from  String to Object prevent from validation error. I was facing the problem for hours and finally I found the solution.

--- models/listing.js
+++ models/listing.js
@@ -8,7 +8,7 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
+    type: Object,  //changes for type string to Objects prevent from giving validation error
     default:
       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     set: (v) =>
